ccf5afe25278d56054d7d54eec34b30daa7b0343
Alastair Coote <alastair.coote@nytimes.com>
2020-11-27 15:46:22 -0500

    some documentation

--- src/args.rs
+++ src/args.rs
@@ -3,12 +3,15 @@ use proc_macro_error::{emit_call_site_error, emit_error};
 use quote::{format_ident, quote_spanned};
 use syn::{spanned::Spanned, Ident, Meta, MetaList, NestedMeta};
 
+/// The actual parsed output of a test_impl macro invocation.
 pub struct TraitAndImpls<'a> {
     pub trait_ident: &'a Ident,
     pub impl_idents: Vec<&'a Ident>,
 }
 
 impl<'a> TraitAndImpls<'a> {
+    /// Output some code that will ensure the trait the user specified actually exists
+    /// in the scope provided, and that it's not a typo, etc etc
     pub fn get_trait_check(&self, fn_ident: &Ident) -> TokenStream {
         let trait_ident = self.trait_ident;
 
@@ -20,6 +23,9 @@ impl<'a> TraitAndImpls<'a> {
         )
     }
 
+    /// Output some code to ensure that the struct names we've been given actually do
+    /// implement the trait we're testing. If they didn't we'd see errors all over the place,
+    /// but adding this check in means we'll just get one error in the attribute field. Much nicer.
     pub fn get_impl_checks(&self, fn_ident: &Ident) -> Vec<TokenStream> {
         let trait_ident = self.trait_ident;
         self.impl_idents
@@ -39,6 +45,8 @@ fn parse_impl_list(arg: &MetaList) -> Option<TraitAndImpls> {
     let trait_ident = match arg.path.get_ident() {
         Some(ident) => ident,
         _ => {
+            // Not sure in what sitatuon this would never have an ident, but according to
+            // the API it's possible, so let's cover our bases:
             emit_error!(
                 arg.span(),
                 "Could not parse this argument, it has no identifier"
@@ -47,6 +55,8 @@ fn parse_impl_list(arg: &MetaList) -> Option<TraitAndImpls> {
         }
     };
 
+    // Apply the same logic to the struct names provided. Maybe the user threw a literal in
+    // there? If so, emit an error yet again.
     let impl_idents: Vec<&Ident> = arg
         .nested
         .iter()
@@ -72,6 +82,8 @@ fn parse_impl_list(arg: &MetaList) -> Option<TraitAndImpls> {
 }
 
 pub fn parse_args<'a>(args: &'a Vec<NestedMeta>) -> Option<TraitAndImpls<'a>> {
+    // The user might have invoked this macro without specifing a trait (i.e. #[test_impl]), which won't
+    // work. So we'll mark an error for that and immediately return:
     let arg = match args.first() {
         Some(march) => march,
         _ => {
@@ -80,13 +92,20 @@ pub fn parse_args<'a>(args: &'a Vec<NestedMeta>) -> Option<TraitAndImpls<'a>> {
         }
     };
 
+    // Similarly, it's a valid macro invocation to specify more than one argument. But we don't support
+    // that (maybe one day? But also, maybe not?) so we'll emit and error and return:
     if args.len() > 1 {
         emit_call_site_error!("You can only specify one trait");
         return None;
     }
 
+    // Not only that (argh), you can specify arguments in a different way than we want (e.g. this = "that"). So again,
+    // if it's not formatted correctly, emit and error and return.
     match arg {
-        NestedMeta::Meta(Meta::List(meta_list)) => parse_impl_list(meta_list),
+        NestedMeta::Meta(Meta::List(meta_list)) => {
+            // But if it IS correct, we can finally actually parse the information we've been given:
+            parse_impl_list(meta_list)
+        }
         _ => {
             emit_error!(
                 arg.span(),
--- src/lib.rs
+++ src/lib.rs
@@ -17,25 +17,42 @@ pub fn test_impl(
     args: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
+    // Parse the arguments parsed to our macro and extract the trait and impl info:
     let args_parsed = parse_macro_input!(args as AttributeArgs);
     let trait_and_impls = match parse_args(&args_parsed) {
+        // If we get None back it's because parse_args has failed and used emit_error.
+        // In this instance we just throw the input stream straight back out again
+        // as there's no further work to do here.
         None => return input,
         Some(t) => t,
     };
+
+    // Now that we've successfully parsed the arguments, parse the actual function we've
+    // been passed.
     let input_parsed = syn::parse::<ItemFn>(input).unwrap();
 
+    // Grab the ident of the function. We use this in the next two calls to ensure that
+    // the type checks we write have unique identifiers, by prepending the fn name to them.
     let fn_ident = &input_parsed.sig.ident;
 
+    // Emit some code to check that the trait the user entered actually exists in this scope
     let trait_check = trait_and_impls.get_trait_check(&fn_ident);
 
+    // Then emit some code to check that the structs we've been given really are implementations
+    // of the trait in question
     let impl_checks = trait_and_impls.get_impl_checks(&fn_ident);
 
+    // Grab some info about the function because I'm not sure how to access it inside quote!
+    // otherwise (e.g. #input_parsed.sig doesn't work)
     let attrs = &input_parsed.attrs;
     let vis = &input_parsed.vis;
     let sig = &input_parsed.sig;
-    let block = &input_parsed.block;
-    let impl_blocks = create_impl_blocks(&trait_and_impls, &block);
 
+    // Take the original function body and copy it multiple times, making the necessary changes
+    // for it to test the implementation rather than the trait itself.
+    let impl_blocks = create_impl_blocks(&trait_and_impls, &input_parsed.block);
+
+    // Finally, reconstruct the whole thing, along with the type check code:
     let output = quote! {
         #trait_check
         #(#impl_checks)*
